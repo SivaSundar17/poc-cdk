ef79b93d882301e13e1904b4c47d3f8c0ff36b54
SivaSundar17 <shivalalitha17@gmail.com>
2024-09-15 11:53:15 +0530

    Initial commit

---
+++ test/cfw-eaa_s-poc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CfwEaaSPoc from '../lib/cfw-eaa_s-poc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cfw-eaa_s-poc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CfwEaaSPoc.CfwEaaSPocStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
